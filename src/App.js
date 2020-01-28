37f39858c550b7b22791a732e2a0582e8c3d964f
Zunaib Imtiaz <zunaib60@gmail.com>
2020-01-28 14:30:34 +0500

    To Github

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from '@zunaib.imtiaz/react-npm-package';
 
 function App() {
   return (
@@ -10,14 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Test className="test">
           Learn React
-        </a>
+        </Test>
       </header>
     </div>
   );
